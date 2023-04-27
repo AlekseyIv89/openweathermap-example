793215d37ca1aef9899abad58e2b1ea883531917
KOMa <alekseyiv89@mail.ru>
2023-04-27 09:27:18 +0300

    Partial use of Lombok to delete boilerplate code

--- src/main/java/ru/alekseyivanov/model/Weather.java
+++ src/main/java/ru/alekseyivanov/model/Weather.java
@@ -1,52 +1,23 @@
 package ru.alekseyivanov.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Основной класс, содержащий погодные данные
  * Можно расширять в зависмости от потребности
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@Getter
+@Setter
 public class Weather {
     private String name;
     private CurrentWeather main;
     private Coordinates coord;
     private Wind wind;
-
-    public Weather() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public CurrentWeather getMain() {
-        return main;
-    }
-
-    public void setMain(CurrentWeather main) {
-        this.main = main;
-    }
-
-    public Coordinates getCoord() {
-        return coord;
-    }
-
-    public void setCoord(Coordinates coord) {
-        this.coord = coord;
-    }
-
-    public Wind getWind() {
-        return wind;
-    }
-
-    public void setWind(Wind wind) {
-        this.wind = wind;
-    }
 
     @Override
     public String toString() {
@@ -54,6 +25,6 @@ public class Weather {
                 "\tТемпература: " + main.getTemp() + " \u00B0C (по ощущениям: " + main.getFeels_like() + " \u00B0C)\n" +
                 "\tВлажность: " + main.getHumidity() + " %\n" +
                 "\tДавление: " + main.getPressure() + " мм рт.ст.\n" +
-                "\tСкорость ветра: " + wind.getSpeed() + " м/с";
+                "\tСкорость ветра: " + wind.getSpeed() + " м/с (порывы до: " + wind.getGust() + " м/с)";
     }
 }
--- src/main/java/ru/alekseyivanov/model/Wind.java
+++ src/main/java/ru/alekseyivanov/model/Wind.java
@@ -1,23 +1,19 @@
 package ru.alekseyivanov.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Класс, содержащий текущие значения по скорости ветра
  * Можно расширять в зависмости от потребности, например добавить направление ветра
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@Getter
+@Setter
 public class Wind {
     private double speed;
-
-    public Wind() {
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
+    private double gust;
 }
